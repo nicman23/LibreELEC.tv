53b553cb2115f3c451aba78d4f26c907348d1535
Ntemis <ierokipides.dem@gmail.com>
2017-07-12 12:36:05 +0300

    S8X2:New project

--- packages/linux-drivers/media_build/package.mk
+++ packages/linux-drivers/media_build/package.mk
@@ -54,7 +54,7 @@ make_target() {
     if [ -f $PKG_DIR/config/generic.config ]; then
       cp $PKG_DIR/config/generic.config v4l/.config
     fi
-  elif [ "$PROJECT" != "S805" ] && [ "$PROJECT" != "S905" ] && [ "$PROJECT" != "S912" ]; then
+  elif [ "$PROJECT" != "S8X2" ] && [ "$PROJECT" != "S805" ] && [ "$PROJECT" != "S905" ] && [ "$PROJECT" != "S912" ]; then
     if [ -f $PKG_DIR/config/usb.config ]; then
       cp $PKG_DIR/config/usb.config v4l/.config
     fi
