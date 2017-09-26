2842457fd6812eb7b05c28b607a1e7da39c42f19
Ntemis <ierokipides.dem@gmail.com>
2017-09-26 18:04:08 +0300

    Add vero2 to build script

--- build.sh
+++ build.sh
@@ -10,8 +10,8 @@ PROJECT=S8X2 SYSTEM=MXIII-1G ARCH=arm make image -j8
 PROJECT=S8X2 SYSTEM=MXIII-PLUS ARCH=arm make image -j8
 PROJECT=S8X2 SYSTEM=X8H-PLUS ARCH=arm make image -j8
 PROJECT=S8X2 SYSTEM=M8S-PLUS ARCH=arm make image -j8
-# >&2 echo "S805.arm"
-# PROJECT=S805 SYSTEM=HD18Q ARCH=arm make image -j8
+# >&2 echo "Vero2.arm"
+PROJECT=S805 SYSTEM=VERO2 ARCH=arm make image -j8
 
 #PROJECT=H3 SYSTEM=opipc ARCH=arm make image -j8
 
